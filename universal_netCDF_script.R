ba8b1f9406df26776f72d98d898c716e9341e1cd
scockrel <scockrel@mix.wvu.edu>
2018-03-31 13:13:02 -0400

    lm working this end; loop to create seasonal matrix now has NA replacement and lm

--- universal_netCDF_script.R
+++ universal_netCDF_script.R
@@ -1,34 +1,37 @@
 
 ## The only potential things that should need changed for use with other netCDF files
 ## 1. Tree ring data file (Line 16)
-## 2. Beginning and ending years for tree ring data (Line 19, numbers)
-## 3. netCDF file name and/or var.name (Line 23, quotes)
-## 4. Spatial extent (Line 28)
-## 5. Line 127-128, naming/order of the raster brick layers; currently for southern hemisphere
-## 6. Color ramp (Line 136)
+## 2. Beginning and ending years for tree ring data (F_yr, L_yr)
+## 3. netCDF file name and/or var.name
+## 4. Spatial extent (ext)
+## 6. Color ramp (col5)
 ## 7. Coastline file, although if packaged with this file it shouldn't need changed.
 rm(list=ls())
-#setwd("C:/Users/S/Desktop/NetCDFRPlay/ERA")
+setwd("C:/Users/S/Dropbox/ATSE Thesis Workspace/ERAclimateExploration")
 
 library(ncdf4)
 library(raster)
 
-#Bring in mean sea level pressure data
-#This file has two variables slp and msl. If only one variable, varname isn't required.
-dat <- brick("Reanal20thc/pres.sfc.mon.mean.nc")#, varname = "tmp")
+# If the netcdf file has one layer, varname isn't required. 
+# If it has more than one, the first will be loaded and give names for all. Use varname="" and 
+# put the variable name abbreviation in "" to load it.
+
+#dat <- brick("Reanal20thc/pres.sfc.mon.mean.nc")#, varname = "tmp") Amy's Data
+dat <- brick("../ERA_Download/era_interim_moda_SLP.nc", varname= 'tcc')
 
 ## set spatial extent for area interested in. Longitude min - max then latitude min - max
 ## Creating this way allows for other created rasters to recognize as an extent with 
 ## the proper spatial extent, otherwise have to set xmin, xmax, ymin, ymax individually.
 
-#ext <- extent(60, 180, -80, -4)
-ext <- extent(-180, 180, -80, 0)
+ext <- extent(60, 180, -80, -4)
+#ext <- extent(-180, 180, -80, 0)
 
 #Spatial crop using extent
 datC <- crop(dat, ext)
 
 ## Tree ring data
-trDat <- read.table("../KBP_South/KBPS_cull_gap.rwl_tabs.txt", header = TRUE)
+trDat <- read.csv("chronos.csv", header = TRUE)
+
 
 ## Decide start year and end year based on target and tree ring data
 #F_yr <- min(as.numeric(substr(names(datC), 2, 5)))
@@ -69,51 +72,28 @@ rm(yr_mo_dy, d)
 datM <- stackApply(datC, yr_season, mean) #raster with mean for each season
 names(datM) <- unique(yr_season) #Is this more efficient since it doesn't have to call on itself?
 
-# If data is not continuous you must
-# replace all NA with a number outside of the bounds of data in order to do correlations.
-# For temperature, 0 will not work since values do go to 0 or below. 
-# Done at this stage because data can be too large.
-# Also subsets seasons at same time
+# Subset season, replace NAs with -9999 for correlation/regression, run linear model extracting residuals.
 # AH: PRETTY!
+# SC: Putting it together - works for spatially complete and incomplete....for me.
 
 for (i in unique(substring(yr_season, 6))){
-  assign(paste0(i), values(subset(datM, grep(i, names(datM), value=T))))
+  d <- values(subset(datM, grep(i, names(datM), value=T)))
+  d[is.na(d[])] <- -9999
+  assign(paste0(i), d)
+  lm_x <- seq(1:dim(get(i))[2])
+  r <- t(resid(lm(t(get(i)) ~ lm_x))) 
+  assign(paste0(i), r)
+  rm(r, d, lm_x)
   }
 
-SON[is.na(SON[])] <- -10
-DJF[is.na(DJF[])] <- -10
-JJA[is.na(JJA[])] <- -10
-MAM[is.na(MAM[])] <- -10
-
-#AH tries to remove trend from each cell in raster
-#Need a sequence of years the right length for each season
-#Need to iterate over each seasonal matrix
-#could be rolled into your loop above, when it works!
-#for DJF
-lm_x <- seq(1:dim(DJF)[2]) #regress over time, exact years not important
-DJF_r <- t(resid(lm(t(DJF)~ lm_x)))
-
-#amy tries a loop and fails!
-for (i in unique(substring(yr_season, 6))){
-  lm_x <- seq(1:dim(get(i))[2])
-  r <- t(resid(lm(t(get(i)) ~ lm_x))) #something is stuffing up here,got it working on DJF alone
-  assign(paste0(i), r) #and maybe here with the names vs matrices
-  rm(r)
-}
-###
-
-#create rasters to ingest the spatial correlations
+#create rasters to store the spatial correlations
 CorT <- setExtent(raster(nrow = nrow(datM), ncol = ncol(datM)),ext)
 Cor <- setExtent(raster(nrow = nrow(datM), ncol = ncol(datM)),ext)
 temp <- setExtent(raster(nrow = nrow(datM), ncol = ncol(datM)),ext)
 
-
-## Presenting the treeCorr Function!!!
-## This compresses multitudes of previous coding into a very small package
 ## Correlation, masking all done in one go. Plot ready after this.
 
-## SC: The following is the combination of previous functions.
-fullCorr <- function(x, y){
+fullCorr <- function(x, y){ # x=climate data, y=column name from trDat in ""
   rng <- range(as.numeric(substr(grep(
     unique(substr(as.character(colnames(x)), 7, 9)), 
     colnames(x), value=T),2, 5)))
@@ -128,22 +108,22 @@ fullCorr <- function(x, y){
   return(temp)
 }
 
-DJF_c <- fullCorr(DJF, "ars")
-MAM_c <- fullCorr(MAM, "ars")
-JJA_c <- fullCorr(JJA, "ars")
-SON_c <- fullCorr(SON, "ars")
+DJF_c <- fullCorr(DJF, "mr_kbp")
+MAM_c <- fullCorr(MAM, "mr_kbp")
+JJA_c <- fullCorr(JJA, "mr_kbp")
+SON_c <- fullCorr(SON, "mr_kbp")
 
 rm(SON, DJF, JJA, MAM, trDat, datM) 
 
 #Stack the new rasters to be displayed.
-Seasons <- stack(DJF_c[[1]], MAM_c[[1]], JJA_c[[1]], SON_c[[1]])
-names(Seasons) <- c("DJF", "MAM", "JJA", "SON")
+Seasons <- stack(SON_c[[1]], DJF_c[[1]], MAM_c[[1]], JJA_c[[1]])
+names(Seasons) <- c("SON", "DJF", "MAM", "JJA")
 
 rm(Cor, CorT, temp)
 #Load in a shapefile and crop for the region of interest 
 library(rgdal)
 library(rgeos)
-coast_shapefile <- crop(readOGR("GISdata/ne_10m_coastline.shp"), ext)
+coast_shapefile <- crop(readOGR("../GISdata/ne_10m_coastline.shp"), ext)
 
 #Create color ramps for mapping and number of colors to use
 library(colorRamps)
