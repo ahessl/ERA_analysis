54b365a2206ec066f0e1ff5fb0930102b91582cf
scockrel <scockrel@mix.wvu.edu>
2018-05-23 22:12:24 -0400

    updated fullCorr in code

--- universal_netCDF_script.R
+++ universal_netCDF_script.R
@@ -132,18 +132,20 @@ temp <- setExtent(raster(nrow = nrow(datM), ncol = ncol(datM)),ext)
 
 ## Correlation, masking all done in one go. Plot ready after this.
 
-fullCorr <- function(r, y){ # x=climate data, y=column name from trDat in ""
-  rng <- c(max(range(as.numeric(substr(colnames(MAM),2, 5)))[1], range((trDat$year))[1]),
-           min(range(as.numeric(substr(colnames(MAM),2, 5)))[2], range((trDat$year))[2]))
-  trYr <-trDat[which(trDat$year >= rng[1] & trDat$year<= rng[2]),]
-  x <- r[,which(substring(colnames(r), 2, 5) >= rng[1] & substring(colnames(r), 2, 5) <= rng[2])]
+fullCorr <- function(ClimMatr, TRInd, TrYrs){ # ClimMatr=climate data matrix, TRInd=column name from trDat in (df$Indices), TrYrs = years (df$years)
+  #Creates date range with tree and climate data max/min years
+  rng <- c(max(range(as.numeric(substr(colnames(ClimMatr),2, 5)))[1], range((TrYrs))[1]),
+           min(range(as.numeric(substr(colnames(ClimMatr),2, 5)))[2], range((TrYrs))[2]))
+  trYr <- setNames(data.frame(TrYrs, TRInd), c("year", "data")) #internal function use -- speeds things up a little more for cor tests
+  trYr <-trYr[which(trYr$year >= rng[1] & trYr$year<= rng[2]),] #crops the tree ring data based on the range; rings can be variable (stupid SH lag thing)
+  x <- ClimMatr[,which(substring(colnames(ClimMatr), 2, 5) >= rng[1] & substring(colnames(ClimMatr), 2, 5) <= rng[2])]
   for(i in 1:dim(x)[1]){
-    Cor[i] <- cor(x=x[i,], y = trYr[,y], method = 'pearson') ## create correlation based on tree ring
-    CorT[i] <- cor.test(x=x[i,], y = trYr[,y], method = 'pearson')$p.value ## p values used to create the cropped confidence intervals
+    Cor[i] <- cor(x=x[i,], y = trYr$data, method = 'pearson') ## create correlation based on tree ring
+    CorT[i] <- cor.test(x=x[i,], y = trYr$data, method = 'pearson')$p.value ## p values used to create the cropped confidence intervals
   }
   CorT[CorT > 0.05] <- NA
-  Cor <- brick(Cor)
-  temp <- mask(Cor, CorT)
+  Cor <- raster::brick(Cor)
+  temp <- raster::mask(Cor, CorT)
   return(temp)
 }
 
